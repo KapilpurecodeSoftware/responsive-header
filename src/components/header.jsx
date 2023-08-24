a61f6e7b7aab57e8ac02da121453982ae12e6016
KapilChoudhary88 <choudharykapil8899@gmail.com>
2023-08-24 17:58:24 +0530

    responsiveness done

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -15,7 +15,7 @@ export default function Header() {
             <ul>
                 <li><a href='#'>Home</a></li>
                 <li><a href='#'>About </a></li>
-                <li><a href='#'>Services</a></li>
+                <li><a href='#'>Service</a></li>
                 <li><a href='#'>Contact</a></li>
                 <li><a href='#'>Feedback</a></li>
             </ul>

02909d77024e2500cd6ee144c9c5565a9468d786
KapilChoudhary88 <choudharykapil8899@gmail.com>
2023-08-24 10:22:07 +0530

    first commit

---
+++ src/components/header.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import logo from "../assets/pureCodeImg.png"
+import "./header.css"
+
+export default function Header() {
+  return (
+    <body>
+        <nav>
+            <input type="checkbox" id='check' />
+            <label htmlFor="check">
+                <i className="fas fa-bars" id='btn'></i>
+                <i class="fas fa-times" id='cancel'></i>
+            </label>
+            <img src={logo}></img>
+            <ul>
+                <li><a href='#'>Home</a></li>
+                <li><a href='#'>About </a></li>
+                <li><a href='#'>Services</a></li>
+                <li><a href='#'>Contact</a></li>
+                <li><a href='#'>Feedback</a></li>
+            </ul>
+        </nav>
+    </body>
+  )
+}
+
+
